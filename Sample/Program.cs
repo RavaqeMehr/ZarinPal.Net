192c13f41c6899048d0fb54dcc89875c68a273da
Moqavem <sina.farahabadi@gmail.com>
2025-05-17 16:30:41 +0330

    feat: create sample

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using ZarinPal.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var appBaseUrl = "https://localhost:3003/";
+
+var zarinpal = builder.Services.AddZarinPal("merchantId", appBaseUrl);
+
+var app = builder.Build();
+
+var testAmount = 100000;
+
+app.MapGet(
+    "/",
+    async () =>
+    {
+        var request = await zarinpal.Request(
+            new RequestRequest(amount: testAmount, description: "تست", mobile: "09123456789")
+        );
+
+        return Results.Redirect(zarinpal.GetPayUrl(request));
+    }
+);
+
+zarinpal.MapZarinPalCallback(
+    app,
+    async (callback) =>
+    {
+        if (callback.Status == CallbackStatus.OK)
+        {
+            var verify = await zarinpal.Verify(new VerifyRequest(testAmount, callback.Authority));
+            if (verify.GetStatus() == VerifyStatus.NotVerified)
+            {
+                return Results.BadRequest("پرداخت انجام نشد!");
+            }
+            return Results.Accepted("پرداخت با موفقیت انجام شد.");
+        }
+        else
+        {
+            return Results.BadRequest("پرداخت انجام نشد!");
+        }
+    }
+);
+
+app.Run(appBaseUrl);
