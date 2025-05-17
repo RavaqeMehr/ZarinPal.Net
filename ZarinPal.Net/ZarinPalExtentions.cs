788b3519a9c0817beb46ca45a16b56d872f4e034
Moqavem <sina.farahabadi@gmail.com>
2025-05-17 16:29:59 +0330

    config nuget

---
+++ ZarinPal.Net/ZarinPalExtentions.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ZarinPal.Net;
+
+public static class ZarinPalExtentions
+{
+    public static ZarinPalClient AddZarinPal(
+        this IServiceCollection services,
+        string merchantId,
+        string baseUrl,
+        string? callbackPath = null
+    )
+    {
+        if (callbackPath is null)
+        {
+            var salt = Guid.NewGuid().ToString().Replace("-", "");
+
+            callbackPath = $"/integrations/zarinpal/{salt}";
+        }
+
+        ZarinPalClient zarinPal = new(merchantId, baseUrl, callbackPath);
+
+        services.AddSingleton(zarinPal);
+
+        return zarinPal;
+    }
+
+    public static void MapZarinPalCallback(
+        this ZarinPalClient zarinPal,
+        WebApplication app,
+        Func<Callback, Task<IResult>> action
+    )
+    {
+        app.MapGet(zarinPal.CallbackPath, ([AsParameters] Callback callback) => action(callback));
+    }
+}
